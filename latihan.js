4af8cb068de67407abad652a72cfd99ad2263f4b
yogaard <yoga.ardiant@gmail.com>
2023-11-22 20:59:38 +0800

    first commit

---
+++ latihan.js
@@ -0,0 +1,65 @@
+// let a = new Date()
+
+// console.log(a.getFullYear())
+
+// function fx(x){
+//     const hasil= x + (x*3)
+//     return hasil
+// }
+
+// function diskon(X){
+//     let musimPandemik =(X* 30)/100
+//     return musimPandemik
+// }
+
+// let sale = diskon(100000)
+// console.log(sale)
+
+
+// function cek  (x,y) {
+//     let hasil;
+//     if  (x > y){
+//         hasil = `${x} lebih besar dari ${y}`
+//     } else  {
+//         hasil = `${y} lebih besar dari ${x}`
+//     }
+//     return hasil
+// }
+// console.log (cek(7,5))
+
+// function evaluasi(score){
+//     let hasil;
+
+//     if(score >= 90){
+//         hasil = `Selamat, Anda mendapatkan nilai A`
+//     }
+//     else if(score >= 80){
+//         hasil = `anda mendapatkan nilai B`
+//     }
+//     else if(score >= 70){
+//         hasil = `anda mendapatkan nilai c`
+//     }
+//     else if(score >= 60){
+//         hasil= `anda mendapatkan nilai D`
+//     }
+//     else if( score < 60){
+//         hasil = `anda mendapatkan nilai E`
+//     }
+//     return hasil
+   
+//     }
+    
+// console.log (evaluasi(4))
+
+// function fx(x,y) {
+//     let hasil = (x*y)
+//     return hasil
+// }
+
+// console.log(fx(4,5))
+
+const myAge=28
+const yourAge=45
+const ourAge=myAge+yourAge;
+console.log(ourAge);
+
